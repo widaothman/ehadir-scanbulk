72dffd9bb697e3a85d509632723ba741213e8adf
widaothman <widaothman11403@gmail.com>
2026-07-17 11:00:52 +0800

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -1,1 +1,39 @@
+function updateCounter(){
 
+    document.getElementById("total").innerText = scanQueue.length;
+
+    document.getElementById("queueCount").innerText = scanQueue.length;
+
+}
+
+function renderTable(){
+
+    const tbody=document.getElementById("scanTable");
+
+    tbody.innerHTML="";
+
+    scanQueue.forEach((student,index)=>{
+
+        tbody.innerHTML +=`
+
+<tr>
+
+<td>${index+1}</td>
+
+<td>${student.id}</td>
+
+<td>${student.nama}</td>
+
+<td>${student.kelas}</td>
+
+<td>${student.status}</td>
+
+<td>${student.masa}</td>
+
+</tr>
+
+`;
+
+    });
+
+}
