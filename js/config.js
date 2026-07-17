15cbbf9c62e99cc7a4b59b36009b4e1f9f4bf516
widaothman <widaothman11403@gmail.com>
2026-07-17 10:59:51 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,24 @@
+const CONFIG = {
 
+    // URL Apps Script (akan diisi kemudian)
+    API_URL: "",
+
+    // Bilangan maksimum rekod dihantar sekali
+    MAX_BATCH: 20,
+
+    // Hantar queue setiap 1 saat
+    SEND_INTERVAL: 1000,
+
+    // Retry jika gagal
+    RETRY_INTERVAL: 3000,
+
+    // Kamera belakang
+    CAMERA_FACING: "environment",
+
+    // FPS scanner 15/20
+    FPS: 15,
+
+    // Saiz kotak QR
+    QRBOX: 250
+
+};
