6e13945b9d80c40614e1ff75a22367197db86494
widaothman <widaothman11403@gmail.com>
2026-07-17 11:00:24 +0800

    Update queue.js

--- js/queue.js
+++ js/queue.js
@@ -1,1 +1,47 @@
+const scanQueue = [];
 
+const scannedIds = new Set();
+
+function isDuplicate(id){
+
+    return scannedIds.has(id);
+
+}
+
+function addQueue(student){
+
+    if(isDuplicate(student.id)){
+
+        return false;
+
+    }
+
+    scannedIds.add(student.id);
+
+    scanQueue.push(student);
+
+    updateCounter();
+
+    renderTable();
+
+    return true;
+
+}
+
+function removeQueue(id){
+
+    const index = scanQueue.findIndex(item => item.id === id);
+
+    if(index >= 0){
+
+        scanQueue.splice(index,1);
+
+    }
+
+}
+
+function getQueue(){
+
+    return scanQueue;
+
+}
