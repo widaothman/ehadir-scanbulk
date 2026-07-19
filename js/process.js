2065a687b32972ee6946d62892fd9028604578a2
widaothman <widaothman11403@gmail.com>
2026-07-19 11:49:43 +0800

    Update process.js

--- js/process.js
+++ js/process.js
@@ -2,18 +2,41 @@ let bil = 1;
 
 function processScan(id){
 
+    // Cari murid dalam RAM
+    const murid = getStudent(id);
+
+    // Jika ID tidak wujud
+    if(!murid){
+
+        alert("ID Murid tidak dijumpai : " + id);
+        return;
+
+    }
+
     const tbody = document.getElementById("scanTable");
 
     const row = tbody.insertRow();
 
+    row.dataset.id = id;
+
     row.insertCell().innerHTML = bil++;
     row.insertCell().innerHTML = id;
-    row.insertCell().innerHTML = "-";
-    row.insertCell().innerHTML = "-";
-    row.insertCell().innerHTML = "Waiting";
+    row.insertCell().innerHTML = murid.nama;
+    row.insertCell().innerHTML = murid.kelas;
+    row.insertCell().innerHTML = "🟡 Sending...";
     row.insertCell().innerHTML =
         new Date().toLocaleTimeString();
 
     document.getElementById("total").innerHTML = bil - 1;
+
+    // Masuk queue
+    addQueue({
+
+        id:id,
+        nama:murid.nama,
+        kelas:murid.kelas,
+        row:row
+
+    });
 
 }
