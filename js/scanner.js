2c722509f9ef093f074d77a5d9c1cbe745bb7cf2
widaothman <widaothman11403@gmail.com>
2026-07-19 20:22:01 +0800

    Update scanner.js

--- js/scanner.js
+++ js/scanner.js
@@ -1,75 +1,51 @@
 const video = document.getElementById("video");
 const hasil = document.getElementById("hasil");
-
 let scanner;
-
 function startScanner() {
-
     scanner = new QrScanner(
-
         video,
-
         result => {
-
             let qrData = result.data
                 ? result.data.trim()
                 : String(result).trim();
-
             // Jika QR mengandungi URL dengan parameter id=
             if (qrData.includes("id=")) {
-
                 try {
-
                     const url = new URL(qrData);
                     qrData = url.searchParams.get("id");
-
                 } catch (e) {
                     console.log(e);
                 }
-
             }
-
            processScan(qrData);
-
             if (hasil) {
                 hasil.innerHTML = "✅ QR : " + qrData;
             }
-
         },
-
         {
             preferredCamera: "environment",
             highlightScanRegion: true,
             highlightCodeOutline: true,
             returnDetailedScanResult: true
         }
-
     );
-
     scanner.start()
         .then(() => {
-
             console.log("✅ Camera Ready");
-
             if (hasil) {
                 hasil.innerHTML = "✅ Kamera aktif. Halakan kepada QR";
             }
-
         })
         .catch(err => {
-
             console.error(err);
-
             if (hasil) {
                 hasil.innerHTML = "❌ " + err;
             }
-
         });
-
 }
-
 window.onload = async function () {
+    alert("1. Scanner mula");
     await loadStudents();
-    alert("Jumlah Murid loaded: " + Object.keys(STUDENTS).length);
+    alert("2. Load selesai");
     startScanner();
-};
+};ini scannr.js
