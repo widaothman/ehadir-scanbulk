687aebbe56a26d6e120558e04767bf06b8d3ece8
widaothman <widaothman11403@gmail.com>
2026-07-19 20:03:26 +0800

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,25 +1,25 @@
 let STUDENTS = {};
 
 async function loadStudents() {
-
     try {
-
         const response = await fetch(
             `${CONFIG.API_URL}?action=students&v=${CONFIG.API_VERSION}`
         );
-
-        STUDENTS = await response.json();
-
+        if (!response.ok) {
+            alert("HTTP Error: " + response.status + " " + response.statusText);
+        }
+        const text = await response.text();
+        try {
+            STUDENTS = JSON.parse(text);
+        } catch (parseErr) {
+            alert("Bukan JSON! Response:\n\n" + text.substring(0, 300));
+            return;
+        }
         console.log("Jumlah Murid :", Object.keys(STUDENTS).length);
-
     } catch (err) {
-
-        console.error("Gagal load murid", err);
-
+        alert("Fetch gagal: " + err.message);
     }
-
 }
-
 function getStudent(id) {
 
     return STUDENTS[id] || null;
