95163711f40cd4d0a10f4376080aa983155e6213
widaothman <widaothman11403@gmail.com>
2026-07-19 17:56:58 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
 
     // URL Apps Script (akan diisi kemudian)
-   API_URL: "https://script.google.com/a/macros/moe-dl.edu.my/s/AKfycbycKEbDLqLzKeV8IFlOF9QCa9wmYJckRNU6QCBCuy5p38tGM393BFAZRwEhOpeN0EKn/exec",
+   API_URL: "https://script.google.com/a/macros/moe-dl.edu.my/AKfycbxSmy6OwY7Ydqt6bVdRlab_HbpDDlmXIZgH2RPlak79nvMLhn_a63T_XUZdzY5bSx0MNg/exec",
     API_VERSION: "1.0",
         
     // Bilangan maksimum rekod dihantar sekali
