c5f70da8c0782ef4383c43ef09f054aa318668e4
widaothman <widaothman11403@gmail.com>
2026-07-19 11:30:13 +0800

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,1 +1,7 @@
+const CONFIG = {
 
+    API_URL: "https://script.google.com/a/macros/moe-dl.edu.my/s/AKfycbycKEbDLqLzKeV8IFlOF9QCa9wmYJckRNU6QCBCuy5p38tGM393BFAZRwEhOpeN0EKn/exec",
+
+    API_VERSION: "1.0"
+
+};
